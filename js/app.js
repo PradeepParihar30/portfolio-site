a2ef5c8e07b85228519724a20869c6c48de8f455
Pradeep Parihar <Pradeepparihar471@gmail.com>
2025-05-20 01:31:39 +0530

    Initial commit for portfolio website

---
+++ js/app.js
@@ -0,0 +1,46 @@
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 60,
+      density: { enable: true, value_area: 800 }
+    },
+    color: { value: "#ffffff" },
+    shape: { type: "circle" },
+    opacity: { value: 0.5 },
+    size: { value: 3, random: true },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 3,
+      direction: "none",
+      out_mode: "out"
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"   // You can try "grab" or "bubble" too
+      },
+      onclick: {
+        enable: true,
+        mode: "push"
+      },
+      resize: true
+    },
+    modes: {
+      repulse: {
+        distance: 100,
+        duration: 0.4
+      }
+    }
+  },
+  retina_detect: true
+});
